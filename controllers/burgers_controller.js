06c37d360535a0af164d07497deadc0dda5e61c9
unknown <mrhdigital@yahoo.com>
2018-06-18 15:56:57 -0500

    working app

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,33 +3,33 @@ var express = require("express");
 var router = express.Router();
 var burger = require("../models/burger.js");
 
-router.get('/', function(req,res) {
-    burger.all(function(burger_data){
-        console.log(burger_data);
-    res.render('index',{burger_data});
+router.get('/', function (req, res) {
+    burger.all(function (burger_data) {
+        //console.log(burger_data);
+        res.render('index', { burger_data });
 
     })
 });
 
 // put route -> back to index
-router.put("/burgers/update", function(req, res) {
-    burger.update(req.body.burger_id, function(result) {
-      // wrapper for orm.js that using MySQL update callback will return a log to console,
-      // render back to index with handle
-      console.log(result);
-      res.redirect("/");
+router.put("/burgers/update", function (req, res) {
+    burger.update(req.body.burger_id, function (result) {
+        // wrapper for orm.js that using MySQL update callback will return a log to console,
+        // render back to index with handle
+        //console.log(result);
+        res.redirect("/");
     });
-  });
+});
 // post route -> back to index
-router.post("/burgers/create", function(req, res) {
+router.post("/burgers/create", function (req, res) {
     // takes the request object using it as input for buger.addBurger
-    burger.create(req.body.burger_name, function(result) {
-      // wrapper for orm.js that using MySQL insert callback will return a log to console,
-      // render back to index with handle
-      console.log(result);
-      res.redirect("/");
+    burger.create(req.body.burger_name, function (result) {
+        // wrapper for orm.js that using MySQL insert callback will return a log to console,
+        // render back to index with handle
+       // console.log(result);
+        res.redirect("/");
     });
-  });
+});
 
 
 module.exports = router;
